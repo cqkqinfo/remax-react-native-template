e3550e43d6bd5a58023bb4544bff3a6294f98e91
mushan <mushan0x0@gmail.com>
2020-05-12 17:48:06 +0800

    Initial commit

--- remax.config.js
+++ remax.config.js
@@ -1,12 +1,5 @@
 const less = require('@remax/plugin-less');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  plugins: [less()],
-  configWebpack({ config }) {
-    // 详细配置参考 copy-webpack-plugin
-    config
-      .plugin('copy')
-      .use(CopyPlugin, [[{ from: 'src/assets', to: 'assets' }]]);
-  }
+  plugins: [less()]
 };
