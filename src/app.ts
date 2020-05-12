4d952c48a067adf93c15773bdc15c4fe25f80429
mushan <mushan0x0@gmail.com>
2020-05-12 17:32:33 +0800

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import './app.less';
+
+const App: React.FC = props => props.children as React.ReactElement;
+
+export default App;
---
+++ src/utils/storage.tsx
@@ -0,0 +1,32 @@
+import {
+  setStorageSync,
+  getStorageSync,
+  removeStorageSync,
+  clearStorageSync
+} from 'remax/wechat';
+
+type keys = 'token' | 'openId';
+
+interface Storage {
+  get: {
+    (key: keys | string): string | null;
+  };
+  set: {
+    (key: keys | string, value: string): void;
+  };
+  del: {
+    (key: keys): void;
+  };
+  clear: () => void;
+}
+
+const storage: Storage = {
+  get: key => getStorageSync(key),
+  set: (key, value: string) => {
+    setStorageSync(key, value);
+  },
+  del: key => removeStorageSync(key),
+  clear: () => clearStorageSync()
+};
+
+export default storage;
