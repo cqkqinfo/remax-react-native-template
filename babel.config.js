d6a7e6a8750d789035da669349491dd7db06bffb
mushan <mushan0x0@gmail.com>
2020-06-29 15:03:45 +0800

    升级包

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+// babel.config.js
+module.exports = {
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'parsec-hooks',
+        camel2DashComponentName: false,
+        customName: name => {
+          return `parsec-hooks/lib/${name
+            .replace(/^(use)/, '')
+            .replace(/^\S/, s => s.toLowerCase())}Hooks`;
+        }
+      }
+    ]
+  ]
+};
