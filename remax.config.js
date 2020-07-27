7fae852cd576adb57d76f2324a67381b0b9d1bb7
mushan <mushan0x0@gmail.com>
2020-07-27 15:32:44 +0800

    更新配置

--- remax.config.js
+++ remax.config.js
@@ -1,5 +1,12 @@
 const less = require('@remax/plugin-less');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  plugins: [less()]
+  plugins: [less()],
+  configWebpack({ config }) {
+    // 详细配置参考 copy-webpack-plugin
+    config
+      .plugin('copy')
+      .use(CopyPlugin, [[{ from: 'src/images', to: 'images' }]]);
+  }
 };
