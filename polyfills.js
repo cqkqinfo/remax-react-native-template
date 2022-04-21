0bd392a52505ba8cb62d718c651f7a974e977e8c
mushan <mushan0x0@gmail.com>
2022-04-21 14:50:12 +0800

    fix: 第三方库样式

--- polyfills.js
+++ polyfills.js
@@ -1,11 +1,15 @@
 // import 'raf/polyfill';
 //
 // console.log(global.requestAnimationFrame);
+import { StyleSheet } from 'react-native';
+import { rpxToPx } from '@kqinfo/ui';
+
 global.navigator = { userAgent: '' };
 global.window = {
   ...global,
   location: {
-    host: ''
+    host: '',
+    href: ''
   }
 };
 
@@ -13,3 +17,25 @@ Object.assign(process.env, {
   REMAX_PLATFORM: 'native'
   // NODE_ENV: 'production',
 });
+
+// require = (...arg) => {
+//   const result = require(...arg);
+//   result.default = result.default || result;
+//   return result;
+// };
+
+StyleSheet.create = styles => {
+  Object.entries(styles).forEach(([key, value]) => {
+    if (typeof value === 'object') {
+      Object.entries(value).forEach(([k, v]) => {
+        if (
+          typeof v === 'number' &&
+          ['width', 'height', 'fontSize', 'lineHeight'].includes(k)
+        ) {
+          styles[key][k] = rpxToPx(v * 2);
+        }
+      });
+    }
+  });
+  return styles;
+};
