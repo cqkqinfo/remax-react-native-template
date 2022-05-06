d0837c44b2f321d18afc09e8db167ce22098c2d3
mushan <mushan0x0@gmail.com>
2022-05-06 10:06:35 +0800

    chore: 更新RN到0.69.0

--- android/app/src/main/java/com/rndemo/MainApplication.java
+++ android/app/src/main/java/com/rndemo/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -32,7 +31,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new RNCWebViewPackage());
           return packages;
         }
 
